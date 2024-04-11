4661ad5bbe68b492e06c77286c4a5b8459e0dec4
ian-michael254 <ian.michael.codes@gmail.com>
2024-04-11 15:18:03 +0300

    outputing javascript

---
+++ output.js
@@ -0,0 +1,9 @@
+//javascript output methods
+//innerhtml
+
+document.getElementById("innerhtml").innerHTML = "This is the example that shows the innerhtml in play";
+
+//document.write() example
+document.write("Example of document .write() method");
+
+//windows.alert()example
